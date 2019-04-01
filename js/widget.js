d7b9422b399504edeb045bb72763e390c7eff1c4
Jimmy L <jimmygam@icloud.com>
2019-04-01 20:48:13 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -8,15 +8,10 @@ class IpPingWidget extends Widget {
 		super.setUp();
 		this.header = true;
 		this.footer = true;
-		this.sizeX = 2;
-		this.sizeY = 1;
-		this.radius = 15;
 	}
 	
 	async ready() {
 		super.ready();
-		
-		//this.controller.GetPing();
 	}
 	
 }
@@ -29,7 +24,6 @@ class IpPingModel extends WidgetModel {
 	
 	setUp() {
 		super.setUp();
-		
 	}
 
 }
@@ -51,23 +45,23 @@ class IpPingView extends WidgetView {
 		SS.style(this.link, {"fontSize": "10px", "textDecoration": "none"});
 		this.stage.appendChild(this.link);*/
 
-		let height = (this.try.mvc.main.header ? 25 : 0) + (this.try.mvc.main.footer ? 25 : 0);
-
+		//label
 		this.try.pingContainer = HH.create("div");
-		SS.style(this.try.pingContainer, {"paddingTop": "10px", "width": "100%", "height": "40%", "lineHeight": "calc(100%)", "textAlign": "center", "fontSize": "55px"});
-		this.try.pingContainer.innerHTML = 0;
+		SS.style(this.try.pingContainer, {"paddingTop": "10px", "width": "100%", "height": "40%", "lineHeight": "calc(100%)", "textAlign": "center", "fontSize": "35px"});
 		this.try.stage.appendChild(this.try.pingContainer);
 
-		this.try.urlContainer = HH.create("input");
-		this.try.urlContainer.setAttribute("type", "text");
-		this.try.urlContainer.setAttribute("value", "1");
-		SS.style(this.try.urlContainer, {"width": "100%", "height": "17%", "textAlign": "center", "fontSize": "14px"});
-		this.try.urlContainer.innerHTML = 0;
-		this.try.stage.appendChild(this.try.urlContainer);
+		//input
+		this.try.ipContainer = HH.create("input");
+		this.try.ipContainer.setAttribute("type", "text");
+		this.try.ipContainer.setAttribute("value", "127.0.0.1");
+		SS.style(this.try.ipContainer, {"width": "100%", "height": "17%", "textAlign": "center", "fontSize": "14px"});
+		this.try.ipContainer.innerHTML = 0;
+		this.try.stage.appendChild(this.try.ipContainer);
 
+		//button
 		this.try.footer.innerHTML = "DÉMARRER";
 		SS.style(this.try.footer, {"fontSize": "16px", "userSelect": "none", "cursor": "pointer"});
-		Events.on(this.try.footer, "click", event => this.try.mvc.controller.GetPing());
+		Events.on(this.try.footer, "click", event => this.try.mvc.controller.GetPing(this.try.ipContainer.value));
 		this.try.stage.appendChild(this.try.footer);
 	}
 	
@@ -89,8 +83,19 @@ class IpPingController extends WidgetController {
 		
 	}
 
-	async GetPing() {
-		this.mvc.view.pingContainer.innerHTML = 10;
+	async GetPing(ip) {
+		this.mvc.view.pingContainer.innerHTML = "34 ms";
+
+		/*var exec = require('child_process').exec;
+		var ping = exec('ping 127.0.0.1');
+		//var ping = exec('cmd /C ping 127.0.0.1');
+		ping.stdout.on('data', function (data) {
+		console.log('' + data);
+		});*/
+
+		let result = await this.try.mvc.main.send("child_process", {"ping" : ip});
+		console.log(result);
+
 	}
 	
 	/*async load() {
