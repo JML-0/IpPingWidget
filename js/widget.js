56be992e0e72d70eec7d717f7f8ca0cc5407ae26
Jimmy L <jimmygam@icloud.com>
2019-04-04 17:33:58 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -100,9 +100,9 @@ class IpPingController extends WidgetController {
 	async load() {
 		let result = await this.mvc.main.dom("https://node.nicopr.fr/dash/ping/ping/www.google.fr"); // load web page
 		let domstr = _atob(result.response.dom); // decode result
-		let parser = new DOMParser(); // init dom parser
-		let dom = parser.parseFromString(domstr, "text/html"); // inject result
-		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[1]/a').firstResult; // find interesting things
+		//let parser = new DOMParser(); // init dom parser
+		//let dom = parser.parseFromString(domstr, "text/html"); // inject result
+		//let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[1]/a').firstResult; // find interesting things
 		//this.mvc.view.update(article.textContent, article.getAttribute("href"));
 
 
@@ -110,12 +110,12 @@ class IpPingController extends WidgetController {
 		console.log('------------------------------------------------------');
 		console.log(domstr);
 		console.log('------------------------------------------------------');
-		console.log(parser);
+		/*console.log(parser);
 		console.log('------------------------------------------------------');
 		console.log(dom);
 		console.log(article);
 		console.log('------------------------------------------------------');
-		console.log(article.textContent);
+		console.log(article.textContent);*/
 		console.log('------------------------------------------------------');
 		console.log('------------------------FIN---------------------------');
 		console.log('------------------------------------------------------');
